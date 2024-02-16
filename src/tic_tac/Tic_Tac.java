f3d500c3e1f00d46fa922c055ab556c9b8bf8d6c
amrinder <amrinder@10.48.10.14>
2024-02-16 14:05:42 -0500

    initial commit 3

---
+++ src/tic_tac/Tic_Tac.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tic_tac;
+
+/**
+ *
+ * @author amrinder
+ */
+public class Tic_Tac {
+
+    /**
+     * author @Amrinder  
+     * editor Gurkirat 
+     * and lovish 
+     * date- 16/2/2024
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
